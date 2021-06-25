31235008d316034b2e087068a593d5c0de46b9b3
Vincent Nguyen <vpvnguyen@gmail.com>
2021-06-24 22:55:57 -0700

    use relative and absolute positioning to place content and tooltip

---
+++ src/ReactTooltipClone/index.js
@@ -0,0 +1,28 @@
+import "./style.css";
+
+export const ReactToolTipClone = () => {
+  return (
+    <>
+      <div className="rtc-root">
+        <div className="rtc-container">
+          <div>Register</div>
+
+          <div className="rtc-tooltip-body rtc-tooltip-custom rtc-tooltip-position rtc-tooltip-show">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+              repellendus iure fugit illum nisi cum qui. Ab optio itaque, quae
+              ut dolorem, quibusdam nesciunt et placeat, provident praesentium
+              possimus accusantium.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
+              ducimus cupiditate odio veniam iusto perspiciatis repellat.
+              Architecto hic, sit molestiae consequuntur temporibus, commodi,
+              iure fuga quo veritatis mollitia ipsam asperiores.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
