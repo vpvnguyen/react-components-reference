51006024726a8f4f0329bacef105ad321a0e2833
Vincent Nguyen <vpvnguyen@gmail.com>
2021-06-26 12:14:51 -0700

    create icon container; tooltip content area

--- src/ReactTooltipClone/index.js
+++ src/ReactTooltipClone/index.js
@@ -5,21 +5,25 @@ export const ReactToolTipClone = () => {
     <>
       <div className="rtc-root">
         <div className="rtc-container">
-          <div>Register</div>
+          <div className="rtc-tooltip-icon">
+            <div>?</div>
+          </div>
 
-          <div className="rtc-tooltip-body rtc-tooltip-custom rtc-tooltip-position rtc-tooltip-show">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-              repellendus iure fugit illum nisi cum qui. Ab optio itaque, quae
-              ut dolorem, quibusdam nesciunt et placeat, provident praesentium
-              possimus accusantium.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
-              ducimus cupiditate odio veniam iusto perspiciatis repellat.
-              Architecto hic, sit molestiae consequuntur temporibus, commodi,
-              iure fuga quo veritatis mollitia ipsam asperiores.
-            </p>
+          <div className="rtc-tooltip-body rtc-tooltip-show">
+            <div className="rtc-tooltip-content">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+                repellendus iure fugit illum nisi cum qui. Ab optio itaque, quae
+                ut dolorem, quibusdam nesciunt et placeat, provident praesentium
+                possimus accusantium.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
+                ducimus cupiditate odio veniam iusto perspiciatis repellat.
+                Architecto hic, sit molestiae consequuntur temporibus, commodi,
+                iure fuga quo veritatis mollitia ipsam asperiores.
+              </p>
+            </div>
           </div>
         </div>
       </div>
