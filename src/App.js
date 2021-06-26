997378cadce6debb910b6609c0bf8bc16a4dbce5
Vincent Nguyen <vpvnguyen@gmail.com>
2021-06-26 12:14:15 -0700

    render react tooltip example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { ReactToolTip } from "./ReactTooltip";
 import { ReactToolTipClone } from "./ReactTooltipClone";
 import { CustomTooltip } from "./CustomTooltip";
+
 function App() {
   return (
     <div>
-      {/* <ReactToolTip /> */}
       <ReactToolTipClone />
       <CustomTooltip />
+      <ReactToolTip />
     </div>
   );
 }
